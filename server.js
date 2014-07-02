25bf6cf3e0989521e02df476cacf59b6d8de9173
markustorok <markus.toeroek@gmail.com>
2014-07-02 17:32:00 +0200

    passport installed and new application structure

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+// set up ======================================================================
+var express = require('express');
+var app = express();
+var http = require('http');
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/instagramclone');
+var passport = require("passport");
 
-var User = mongoose.model('User', {name: String});
+var morgan = require("morgan");
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var user = new User({name: "Markus"});
-user.save(function (err) {
-    if (err) {
-        console.log('failed');
-    } else {
-        console.log('saved');
-    }
-})
+app.set('port', 3000);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// routes ======================================================================
+require('./lib/routes.js')(app);
+
+// launch ======================================================================
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
