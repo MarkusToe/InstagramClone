0810e52d4d4c56753ea905339bc22d892a010844
markustorok <markus.toeroek@gmail.com>
2014-07-03 10:44:26 +0200

    sessions integrated

--- server.js
+++ server.js
@@ -10,15 +10,24 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+mongoose.connect('mongodb://localhost/instagramclone');
+
 app.set('port', 3000);
 
-app.use(morgan('dev'));
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
+
+// required for passport
+app.use(session({ secret: 'officialdoctorchemicalworld' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
