1b75435dad2fed7491af7c72785d4ade7762a27c
markustorok <markus.toeroek@gmail.com>
2014-07-05 19:56:26 +0200

    passport configuration finished

--- server.js
+++ server.js
@@ -1,43 +1,42 @@
 // set up ======================================================================
 var express = require('express');
 var app = express();
-var http = require('http');
+var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
 var passport = require("passport");
+var flash = require("connect-flash");
 
 var morgan = require("morgan");
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-mongoose.connect('mongodb://localhost/instagramclone');
+var configDB = require('./config/database.js');
 
-app.set('port', 3000);
+// configuration ===============================================================
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport); // pass passport for configuration
 
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-app.use(express.static(__dirname + '/public'));
-app.use(function (req, res, next) {
-    res.locals.showTests = app.get('env') !== 'production' &&
-        req.query.test === '1';
-    next();
-});
-
-// required for passport
-app.use(session({ secret: 'officialdoctorchemicalworld' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+//app.use(express.static(__dirname + '/public'));
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// required for passport
+app.use(session({ secret: 'officialdoctorchemicalworld' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // routes ======================================================================
-require('./lib/routes.js')(app);
+require('./lib/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+app.listen(port);
+console.log('The magic happens on port ' + port);
