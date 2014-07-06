2e214ceba10ed738c3eb110b8eef0013051ce89b
markustorok <markus.toeroek@gmail.com>
2014-07-06 11:23:30 +0200

    file upload started

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
 var passport = require("passport");
 var flash = require("connect-flash");
+var fs = require("fs");
+var multer = require("multer");
 
 var morgan = require("morgan");
 var cookieParser = require('cookie-parser');
@@ -21,7 +23,8 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.use(multer({ dest: './public/uploads/'}))
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
@@ -35,7 +38,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./lib/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./lib/routes.js')(app, passport, fs); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
