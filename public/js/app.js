a0a0340e8bd13f45325f12d50bc88e185d45ceaf
markustorok <markus.toeroek@gmail.com>
2014-07-07 11:49:59 +0200

    user images displayed on profile

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
 var instagramclone = angular.module("instagramclone", []);
 
-instagramclone.controller("AppCtrl", function ($http) {
+instagramclone.controller("AppCtrl", function ($scope, $http) {
     var app = this;
     $http.get("http://localhost:3000/images").success(function (images) {
         app.images = images;
-    })
+    });
+
+    $scope.makeGallery = function (user) {
+        console.log("in the function");
+        $http.get("http://localhost:3000/images/" + user).success(function (images) {
+            app.userImages = images;
+        });
+    }
 });
+
+instagramclone.directive("gallery", function () {
+    return function (scope, element, attrs) {
+        scope.$apply("makeGallery({{attrs.user}})")
+    }
+})
